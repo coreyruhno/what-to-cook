1b983405728678f53b0c49ddd020b5d06445071f
DrCorey <coreyruhno@gmail.com>
2024-03-15 10:08:43 -0400

    Initial Commit

---
+++ What to Cook/What_to_CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_to_CookApp.swift
+//  What to Cook
+//
+//  Created by Corey Ruhno on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct What_to_CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
